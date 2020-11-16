9c4f123dc5a4d860cc1efe3337ee27e3125e3bfa
Mathman <17e23052@marling.gloucs.sch.uk>
2020-11-16 12:07:56 +0000

    Odd or even

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from random import randrange
 dice_1 = randrange(1,7)
 dice_2 = randrange(1,7)
-print(f"You rolled a {dice_1} and a {dice_2}")
+if dice_1 == 1 or dice_1 == 3 or dice_1 == 5:
+  print(f"You rolled a {dice_1} which is odd")
+elif dice_1 == 2 or dice_1 == 4 or dice_1 == 6:
+  print(f"You rolled a {dice_1} which is even")
+if dice_2 == 1 or dice_2 == 3 or dice_2 == 5:
+  print(f"You rolled a {dice_2} which is odd")
+elif dice_2 == 2 or dice_2 == 4 or dice_2 == 6:
+  print(f"You rolled a {dice_2} which is even")
 if dice_1 == dice_2:
   print("You rolled a double!")
 else:
